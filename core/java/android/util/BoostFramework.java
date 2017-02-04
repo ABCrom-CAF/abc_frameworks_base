6f9e50d2c10cac7942487fe9fb01b1cfc16c1139
Alex Naidis <alex.naidis@linux.com>
2017-02-04 21:37:21 +0100

    BoostFramework: Implement asynchronous processing of perflocks
    The path that a perflock request
    from framework has to go to be queued
    to mp-ctl looks as follows:
    
          (reflection)   (native call)       (dynamic link)
    Framework -> QPerformance -> QPerformance JNI ->
    
                  (direct)
    -> mp-ctl client -> mp-ctl
    
    This is a long path and processing the perflock
    requests synchronously causes a delay
    in the code execution of the caller.
    
    This is critical since most callers
    rely on a quick processing time.
    
    As solution, we move the perflock
    requests to asynchronous processing.
    
    As side effect we have to drop
    the return values.
    That is fine since the return
    values are never handled anyways.
    
    IOP requests should still be processed
    synchronously to ensure that prefetching
    is actually completed in time.
    That's no problem since the case
    of the app launch is no latency-critical
    case anyways.
    
    Change-Id: I080fa3a15b0eaae201eff71460eebd1feb64a78b
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Wight554 <wight554@gmail.com>

--- core/java/android/util/BoostFramework.java
+++ core/java/android/util/BoostFramework.java
@@ -129,27 +129,25 @@ public class BoostFramework {
     }
 
 /** @hide */
-    public int perfLockAcquire(int duration, int... list) {
-        int ret = -1;
-        try {
-            Object retVal = mAcquireFunc.invoke(mPerf, duration, list);
-            ret = (int)retVal;
-        } catch(Exception e) {
-            Log.e(TAG,"Exception " + e);
-        }
-        return ret;
+    public void perfLockAcquire(int duration, int... list) {
+        new Thread(() -> {
+            try {
+                mAcquireFunc.invoke(mPerf, duration, list);
+            } catch(Exception e) {
+                Log.e(TAG,"Exception " + e);
+            }
+        }).start();
     }
 
 /** @hide */
-    public int perfLockRelease() {
-        int ret = -1;
-        try {
-            Object retVal = mReleaseFunc.invoke(mPerf);
-            ret = (int)retVal;
-        } catch(Exception e) {
-            Log.e(TAG,"Exception " + e);
-        }
-        return ret;
+    public void perfLockRelease() {
+        new Thread(() -> {
+            try {
+                mReleaseFunc.invoke(mPerf);
+            } catch(Exception e) {
+                Log.e(TAG,"Exception " + e);
+            }
+        }).start();
     }
 
 /** @hide */
@@ -158,7 +156,7 @@ public class BoostFramework {
         try {
             Object retVal = mReleaseHandlerFunc.invoke(mPerf, handle);
             ret = (int)retVal;
-        } catch(Exception e) {
+        } catch(Exception e) {  
             Log.e(TAG,"Exception " + e);
         }
         return ret;
